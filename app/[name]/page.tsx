9827cdce72317af44c5bc4ea2e2a9fae8da2f4f1
Philbert Shea <philbertshea@gmail.com>
2025-06-15 01:33:31 +0800

    Update code in [name]/page.tsx to support dynamic names

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -1,17 +1,27 @@
-'use client';
 import data from "@/data.json";
 import {InfoBlock} from "@/app/utils/helpers";
 
-export default function Home() {
-  return (
-    <>
-      <div className="flex place-items-center flex-col mx-auto w-full justify-center mt-6 p-2">
-        <h1 className="font-bold mt-4 text-5xl">Hello, {data.name}.</h1>
-        <h2 className="text-xl mt-3 pl-5 sm:pl-56 pr-5 sm:pr-56">{data.description}</h2>
-        {data.schemes.map((scheme) => (
-          <InfoBlock title={scheme.title} links={scheme.links} />
-        ))}
-      </div>
-    </>
-  );
+export default async function HomeWithName({ params }: {params: Promise<{ name: string }>}) {
+    const { name } = await params;
+    let processedName: string = name;
+
+    if (name.startsWith("mr")) {
+        processedName = "Mr. " + name.charAt(2).toUpperCase() + name.substring(3);
+    } else if (name.startsWith("ms")) {
+        processedName = "Ms. " + name.charAt(2).toUpperCase() + name.substring(3);
+    } else if (name.startsWith("mdm")) {
+        processedName = "Mdm. " + name.charAt(3).toUpperCase() + name.substring(4);
+    }
+
+    return (
+        <>
+        <div className="flex place-items-center flex-col mx-auto w-full justify-center mt-6 p-2">
+            <h1 className="font-bold mt-4 text-5xl">Hello, {processedName}.</h1>
+            <h2 className="text-xl mt-3 pl-5 sm:pl-56 pr-5 sm:pr-56">{data.description}</h2>
+            {data.schemes.map((scheme) => (
+            <InfoBlock title={scheme.title} links={scheme.links} />
+            ))}
+        </div>
+        </>
+    );
 }
